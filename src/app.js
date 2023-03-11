f444c7d4618e918454a7465d3d59feaa5b468f17
Juan Penalver <juanpenalversanchez11@gmail.com>
2023-03-11 17:36:42 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+};
+
+let excuseGenerator = () => {
+  let article = ["The", "A"];
+  let noun = ["jogger", "dog", "dolphin", "umbrella"];
+  let verb = ["is"];
+  let gerundif = ["running", "barking", "red", "swimming"];
+
+  let articleIndex = Math.floor(Math.random() * article.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let gerundifIndex = Math.floor(Math.random() * gerundif.length);
+
+  return (
+    article[articleIndex] +
+    " " +
+    noun[nounIndex] +
+    " " +
+    verb[verbIndex] +
+    " " +
+    gerundif[gerundifIndex]
+  );
 };
